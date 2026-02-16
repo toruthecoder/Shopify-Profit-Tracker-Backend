4f3077c5c857cdfd8bb350e711e029e7f1242253
toruthecoder <devishsage@gmail.com>
2026-02-16 21:31:10 +0500

    push

--- src/contollers/fetchData.js
+++ src/contollers/fetchData.js
@@ -9,12 +9,76 @@ export const fetchData = async (req, res) => {
         const returns = await Return.find({ shop })
         const products = await Product.find({})
 
+        // sales result
+        const salesResult = orders.reduce((sum, order) => {
+            return sum + parseFloat(order.totalPrice || 0)
+        }, 0)
+
+        // cutomer result
+        const uniqeCustomer = new Set(
+            orders.map(order => order?.email).filter(id => id)
+        )
+        const totalCustomer = uniqeCustomer.size
+
+        // discount result 
+        const totalDiscounts = orders.reduce((sum, order) => {
+            return sum + parseFloat(order?.rawData?.total_discounts || 0)
+        }, 0)
+
+        // refunds results
+        const totalRefundCosts = returns.reduce((sum, refund) => {
+            return sum + parseFloat(refund?.totalRefunded)
+        }, 0)
+
+        // revenue results
+        const totalRevenue = salesResult - totalDiscounts - totalRefundCosts
+
+        // const totalCosts =
+        //     totalCOGS +
+        //     totalShipping +
+        //     totalPaymentFees +
+        //     totalTransactionFees +
+        //     totalRefundCosts;
+
+        // Total Expenses =
+        //     Total COGS
+        //   + Total Payment Fees
+        //   + Total Shipping Cost
+        //   + Total Refunds
+        //   + Total Discounts
+        //   + Fixed Monthly Costs (apps + Shopify plan)
+        //   + Marketing Spend
+
+        // Shipping results
+        const totalShipping = orders.reduce((sum, order) => {
+            return sum + parseFloat(order?.rawData?.total_shipping_price_set?.shop_money?.amount)
+        }, 0)
+
+        // total costs results
+        const totalCosts = salesResult + totalShipping + totalRefundCosts;
+
+        // total expenses result
+        const totalExpenses = totalCosts + totalDiscounts;
+
+        // total netprofit
+        const netProfit = totalRevenue - totalExpenses
+
+        // total profitMargin
+        const profitMargin = (netProfit / totalRevenue) * 100;
         res.json({
             success: true,
             stats: {
                 totalOrders: orders.length,
                 totalReturns: returns.length,
                 totalProducts: products.length,
+                salesResult,
+                totalCustomer,
+                totalRevenue,
+                totalCosts,
+                totalRefundCosts,
+                totalExpenses,
+                netProfit,
+                profitMargin,
             },
             orders,
             returns,
